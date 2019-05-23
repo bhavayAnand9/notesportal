98f43b5ef586cfcb7fb479b7ddcac7fcf10fa503
Bhavay Anand <bhavayanandcse@gmail.com>
2019-05-23 13:44:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: '5000',
-    MONGODB_URI: 'mongodb+srv://bhavayAnand9:qazPLM2222@cluster0-ulyqb.mongodb.net/test?retryWrites=true',
+    MONGODB_URI: '',
     // MONGODB_URI: 'mongodb://localhost:27017/notes'
-    sendGrid_API_key :'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',
+    sendGrid_API_key :'',
     SECRET_KEY: 'notes portal ipu bhavayAnand9 nodejss'
 }
